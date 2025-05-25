3ba30ce33797cef47642026a077988da084dcf87
tom-heidenreich <tom.heidenreich@uni-ulm.de>
2025-05-25 16:38:51 +0200

    feat: use protonwire provider

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,11 @@
-import { $, sleep } from "bun";
-import { getForwardedPort, QBitClient } from "./utils";
+import { QBitClient } from "./utils/qbit";
+import Protonwire from "./provider/protonwire";
 
 // login to qbit client
 const client = new QBitClient();
-const api = await client.auth()
-console.log('Successfull login to client!');
+const api = await client.auth();
+console.log("Successfull login to client!");
 
-while(true) {
-    try {
-        const port = await getForwardedPort()
-        console.log(`Port Forward enabled at port ${port}`);
-        
-        await api.setListenPort(port)
-        console.log(`Updated listenPort successfully!`);
-
-        console.log(`Will renew in 45 seconds...`);
-        await sleep(45000)
-
-    } catch(e) {
-        console.log(`Encounter error: ${e}. Retrying in 20 seconds...`);
-    }
-}
+// start provider
+const protonwire = new Protonwire(api);
+protonwire.start();
