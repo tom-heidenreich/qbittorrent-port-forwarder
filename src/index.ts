20ff85da3075479a24f9824c11ce2dcebc77c732
tom-heidenreich <tom.heidenreich@uni-ulm.de>
2025-05-25 17:37:16 +0200

    feat: select provider by env var

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import { QBitClient } from "./utils/qbit";
 import Protonwire from "./provider/protonwire";
+import Gluetun from "./provider/gluetun";
+import { env } from "./utils/env";
+import { z } from "zod/v4";
+import type Provider from "./provider";
 
 // login to qbit client
 const client = new QBitClient();
 const api = await client.auth();
 console.log("Successfull login to client!");
 
+// select provider
+const ProviderNames = ["protonwire", "gluetun"] as const;
+const PROVIDERS: Record<(typeof ProviderNames)[number], Provider> = {
+  protonwire: new Protonwire(api),
+  gluetun: new Gluetun(api),
+};
+const provider = PROVIDERS[env("PROVIDER", z.enum(ProviderNames).parse)];
+
 // start provider
-const protonwire = new Protonwire(api);
-protonwire.start();
+provider.start();
