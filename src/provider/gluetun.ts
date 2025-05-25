0bf722fd623a0544cdf67226aa793b29d53fed40
tom-heidenreich <tom.heidenreich@uni-ulm.de>
2025-05-25 17:55:54 +0200

    feat: add untested version for gluetun

--- src/provider/gluetun.ts
+++ src/provider/gluetun.ts
@@ -2,10 +2,24 @@ import { $ } from "bun";
 import Provider from ".";
 import { type QBitConnection } from "../utils/qbit";
 
+async function getForwardedPort() {
+  return (
+    await $`curl -s http://$VPN_HOST/v1/openvpn/portforwarded | jq -r .port`.text()
+  ).trim();
+}
+
 export default class Gluetun extends Provider {
   public constructor(qbit: QBitConnection, interval?: number) {
     super(qbit, interval || 45000);
   }
 
-  protected async execute() {}
+  protected async execute() {
+    const port = await getForwardedPort();
+    console.log(`Port Forward enabled at port ${port}`);
+
+    await this.qbit.setListenPort(port);
+    console.log(`Updated listenPort successfully!`);
+
+    console.log(`Will renew in ${Math.floor(this.interval / 1000)} seconds...`);
+  }
 }
